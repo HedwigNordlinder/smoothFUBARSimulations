a4170710b7ef300ec61e14755cae10133853a7ba
HedwigNordlinder <160422581+HedwigNordlinder@users.noreply.github.com>
2025-06-30 11:41:13 +0200

    adding FIFE also

--- testscript.jl
+++ testscript.jl
@@ -77,5 +77,5 @@ df = create_simulation_parameter_csv("simulation_parameters.csv")
 
 # Run the batch simulation
 run_simulation_batch("simulation_parameters.csv", "simulations")
-run_fubar_benchmark("simulations/",[DirichletFUBAR(), SKBDIFUBAR()])
+run_fubar_benchmark("simulations/",[DirichletFUBAR(), SKBDIFUBAR(), FIFEFUBAR()])
 generate_roc_curves("simulations/")
