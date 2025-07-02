3c36ea20e1f9dc54dec28012290286195bdc18ca
HedwigNordlinder <160422581+HedwigNordlinder@users.noreply.github.com>
2025-07-02 12:12:32 +0200

    small sim to see why the report generation messed up

--- testscript.jl
+++ testscript.jl
@@ -11,15 +11,17 @@ include("plot_simulation_data.jl")
 
 function create_simulation_parameter_csv(output_file::String = "simulation_parameters.csv")
     # Parameter combinations to explore
-    #ntaxa_values = [10, 20, 50, 100, 200]
-    ntaxa_values = [20,100,500]
-    # nsites_values = [100, 500, 1000]
-    nsites_values = [100,500,2500]
-    # diversifying_sites_values = [1, 10, 50]
-    diversifying_sites_values = [0.01,0.05,0.1,0.2]
+    #ntaxa_values = [20,100,500]
+    ntaxa_values = [20,100]
+    #nsites_values = [100,500,2500]
+    nsites_values = [100,500]
+
+    #diversifying_sites_values = [0.01,0.05,0.1,0.2]
+    diversifying_sites_values = [0.01]
     scenarios = ["LogisticScenario()", "SeasonalScenario(;sin_divisor=1.0)"]
     normalisations = [30.0,60.0,120.0]
-    λs = [0.5,1.0,2.0]
+    #λs = [0.5,1.0,2.0]
+    λs = [1.0]
     # Static parameters
     static_params = (
         nucleotide_model = "default",
